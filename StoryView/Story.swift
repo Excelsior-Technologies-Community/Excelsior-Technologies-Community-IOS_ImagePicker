f571f1b5086c67d04e7a01ab6b7d17c9f18fe637
noman1303 <belimnoman1303@gmail.com>
2025-12-08 00:19:24 +0530

    Testing

---
+++ StoryView/Story.swift
@@ -0,0 +1,12 @@
+//
+//  Story.swift
+//  StoryView
+//
+//  Created by Noman belim on 07/12/25.
+//
+
+import Foundation
+import SwiftUI
+
+
+ 
---
+++ StoryView/StoryViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryViewApp.swift
+//  StoryView
+//
+//  Created by Noman belim on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StoryViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
