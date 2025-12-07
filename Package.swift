dfa674c74eaa4d062987d4b168b81bf69412e2d6
noman1303 <belimnoman1303@gmail.com>
2025-12-08 00:28:45 +0530

    Version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ExcelsiorTechnologiesImagePicker",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         .library(
